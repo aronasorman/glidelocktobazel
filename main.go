8f2eef7d969825903271e330d8950f9744d60332
Aron Fyodor Asor <aronasorman@gmail.com>
2017-08-19 00:05:11 -0700

    first working prototype

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/Masterminds/glide/cfg"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
+
+var goRepoTemplate = `
+go_repository(
+    name="%s",
+    importpath="%s",
+    commit="%s",
+)
+)
+`
 
 func main() {
 
@@ -18,13 +26,46 @@ func main() {
 
 	filename := os.Args[1]
 
-	out, err := ioutil.ReadFile(filename)
+	lockfile, err := cfg.ReadLockFile(filename)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 
-	lockfile := cfg.Lockfile{}
+	for _, imports := range lockfile.Imports {
+		// do the easy parts of the go_repository first
+		bazelImportpath := imports.Name
+		bazelVersion := imports.Version
 
-	yaml.Unmarshal(out, &lockfile)
+		// now for the slightly involved part -- generating the bazel repo name.
+		// For an example, it transforms the following string like this:
+		// github.com/Masterminds/vcs -> com_github_masterminds_vcs
+		// it takes the domain, then inverts it so that the TLD goes first.
 
+		// first, let's divide it by the slash (/), separating the domain and the urlpath
+		// we should have exactly three divisions
+		splits := strings.SplitAfterN(imports.Name, "/", 2)
+
+		if len(splits) != 2 {
+			panic(fmt.Sprintf("One of the repo names is invalid :%v", imports.Name))
+		}
+
+		fulldomain, urlpath := splits[0], splits[1]
+
+		// just slugify the urlpath
+		slugURLPath := strings.Replace(urlpath, ".", "_", -1)
+		slugURLPath = strings.Replace(slugURLPath, "/", "_", -1)
+		slugURLPath = strings.ToLower(slugURLPath)
+
+		// further split the domain into the TLD and the actual domain
+		domainsplit := strings.Split(fulldomain, ".")
+		domain, tld := domainsplit[0], domainsplit[1]
+		// remove the slash from the tld that Split leaves behind
+		tld = strings.Replace(tld, "/", "", -1)
+
+		// put everything together
+		bazelName := fmt.Sprintf("%s_%s_%s", tld, domain, slugURLPath)
+
+		// print everything
+		fmt.Printf(goRepoTemplate, bazelName, bazelImportpath, bazelVersion)
+	}
 }
